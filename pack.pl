8f06e170dcd6fba7433783825271171cf7445c6d
Jan Wielemaker <J.Wielemaker@vu.nl>
2023-07-16 13:58:44 +0200

    Turn into git managed pack

--- pack.pl
+++ pack.pl
@@ -6,4 +6,4 @@ author('Jan Wielemaker Liang', 'jan@swi-prolog.org').
 packager('Jan Wielemaker Liang', 'jan@swi-prolog.org').
 maintainer('Jan Wielemaker Liang', 'jan@swi-prolog.org').
 home('https://github.com/JanWielemaker/ddebug').
-download('https://github.com/JanWielemaker/ddebug/archive/v0.1.zip').
+download('https://github.com/JanWielemaker/ddebug.git').
